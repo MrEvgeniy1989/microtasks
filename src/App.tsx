89992714ca110f9f5481a58ac13d5dbfe0cccea9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-14 14:29:50 +0300

    4. useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import {MouseEvent} from 'react';
 import './App.css';
-import {Button} from './3/components/Button';
+// import {Button} from './3/components/Button';
 // import {Header} from './1/Header';
 // import {Body} from './1/Body';
 // import {Footer} from './1/Footer';
@@ -41,34 +41,55 @@ function App() {
     // }
 
 
-    const Button1Foo = (subscriber:string, age:number, address: string) => {
-        console.log(subscriber, age, address)
+    // const Button1Foo = (subscriber:string, age:number, address: string) => {
+    //     console.log(subscriber, age, address)
+    // }
+    // const Button2Foo = (subscriber:string) => {
+    //     console.log(subscriber)
+    // }
+    // const Button3Foo = () => {
+    //     console.log("I am stupid button")
+    // }
+
+    // return (
+    //     <div className={'App'}>
+    //         {/*<Header titleForHeader = {"New Header"}/>*/}
+    //         {/*<Body titleForBody = {"New Body"}/>*/}
+    //         {/*<Footer titleForFooter = {"New Footer"}/>*/}
+    //         {/*<NewComponent students={students}/>*/}
+    //         {/*<button onClick={(event)=>{console.log("Hello")}}>MyYouTubeChannel-1</button>*/}
+    //         {/*<button onClick={foo1}>1</button>*/}
+    //         {/*<button onClick={((event: MouseEvent<HTMLButtonElement>) => foo2(100200))}>2</button>*/}
+    //         {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('VASYA')}> MyYouTubeChannel-1*/}
+    //         {/*</button>*/}
+    //         {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('IVAN')}>MyYouTubeChannel-2*/}
+    //         {/*</button>*/}
+    //
+    //         {/*<Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("I am Vasya", 21, "live in Minsk")}/>*/}
+    //         {/*<Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Ivan")}/>*/}
+    //         {/*<Button name={"stupid button"} callBack={Button3Foo}/>*/}
+    //     </div>
+
+    // let a = 1
+
+    let [a, setA] = useState(1)
+
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
     }
-    const Button2Foo = (subscriber:string) => {
-        console.log(subscriber)
+
+    const onClickHandler1 = () => {
+        a=0
+        setA(a)
+        console.log(a)
     }
-    const Button3Foo = () => {
-        console.log("I am stupid button")
-    }
-    
+
     return (
-        <div className={'App'}>
-            {/*<Header titleForHeader = {"New Header"}/>*/}
-            {/*<Body titleForBody = {"New Body"}/>*/}
-            {/*<Footer titleForFooter = {"New Footer"}/>*/}
-            {/*<NewComponent students={students}/>*/}
-            {/*<button onClick={(event)=>{console.log("Hello")}}>MyYouTubeChannel-1</button>*/}
-            {/*<button onClick={foo1}>1</button>*/}
-            {/*<button onClick={((event: MouseEvent<HTMLButtonElement>) => foo2(100200))}>2</button>*/}
-            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('VASYA')}> MyYouTubeChannel-1*/}
-            {/*</button>*/}
-            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('IVAN')}>MyYouTubeChannel-2*/}
-            {/*</button>*/}
-
-            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("I am Vasya", 21, "live in Minsk")}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Ivan")}/>
-            <Button name={"stupid button"} callBack={Button3Foo}/>
-
+        <div className="App">
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler1}>0</button>
         </div>
     );
 }
